208143cbc6b6655eec5fa9140cb70c0e266c3c6d
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-10 00:33:17 +0300

    build: pin Java 25 Gradle toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 group = "com.samo"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
 repositories {
     mavenCentral()
 }

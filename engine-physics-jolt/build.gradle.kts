820ed6c3a55d47246aa9dd9ab0f4c2dbfc8995fc
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-10 12:39:19 +0300

    build: inherit shared Java conventions from root

--- engine-physics-jolt/build.gradle.kts
+++ engine-physics-jolt/build.gradle.kts
@@ -2,19 +2,6 @@ plugins {
     `java-library`
 }
 
-group = "com.samo"
-version = "1.0-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
-    }
-}
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(project(":engine-core"))
 }

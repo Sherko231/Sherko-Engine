091a819116af3a72eb401dac95888465ca77d9a0
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-10 11:06:09 +0300

    P1-T01: add game-server module

---
+++ game-server/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+}
+
+group = "com.samo"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":engine-core"))
+}

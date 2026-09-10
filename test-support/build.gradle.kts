ecad9ce208cea203dc80298d54c4acdd2c02e899
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-10 11:05:52 +0300

    P1-T01: add test-support module

---
+++ test-support/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `java-library`
+}
+
+group = "com.samo"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}

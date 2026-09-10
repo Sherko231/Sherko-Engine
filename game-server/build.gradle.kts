0ff22252779f00bd7dd101b5decebdd54d4e3c9a
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-10 18:15:09 +0300

    build: make version property loading explicit

--- game-server/build.gradle.kts
+++ game-server/build.gradle.kts
@@ -22,7 +22,7 @@ val forbiddenHeadlessArtifacts = listOf(
 )
 
 val compatibilityVersions = Properties().apply {
-    rootProject.file("config/version.properties").inputStream().use(::load)
+    rootProject.file("config/version.properties").inputStream().use { load(it) }
 }
 val engineCommit = providers.exec {
     workingDir(rootDir)

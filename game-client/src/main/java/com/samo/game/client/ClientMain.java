37f6cf8d6e01b2357f0bf889d1b74ecb838ebf19
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-20 19:57:00 +0300

    [P5R-T19] Update client version report reference

--- game-client/src/main/java/com/samo/game/client/ClientMain.java
+++ game-client/src/main/java/com/samo/game/client/ClientMain.java
@@ -1,6 +1,6 @@
 package com.samo.game.client;
 
-import com.samo.game.client.internal.VersionReport;
+import com.samo.game.client.internal.ClientVersionReport;
 
 public final class ClientMain {
     private ClientMain() {
@@ -8,7 +8,7 @@ public final class ClientMain {
 
     public static void main(String[] args) {
         if (args.length == 1 && "--version".equals(args[0])) {
-            VersionReport.print();
+            ClientVersionReport.print();
             return;
         }
 

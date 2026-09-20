3318851e2ffeb81422bd9a34389eca0a9df5e388
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-21 01:09:35 +0300

    [PRE-P6] Preserve visual-demo shader text blocks

--- engine-render-opengl/src/visualDemo/java/com/samo/engine/render/opengl/internal/MaterialComparisonOverlay.java
+++ engine-render-opengl/src/visualDemo/java/com/samo/engine/render/opengl/internal/MaterialComparisonOverlay.java
@@ -13,36 +13,36 @@ final class MaterialComparisonOverlay implements AutoCloseable {
     private static final int INDEX_BYTES = 6 * Integer.BYTES;
 
     private static final String VERTEX_SHADER = """
-        #version 460 core
-        layout(location = 0) in vec3 position;
-        layout(location = 1) uniform float horizontalOffset;
-        void main() {
-            gl_Position = vec4(position.x + horizontalOffset, position.y, position.z, 1.0);
-        }
-        """;
+            #version 460 core
+            layout(location = 0) in vec3 position;
+            layout(location = 1) uniform float horizontalOffset;
+            void main() {
+                gl_Position = vec4(position.x + horizontalOffset, position.y, position.z, 1.0);
+            }
+            """;
 
     private static final String FRAGMENT_SHADER = """
-        #version 460 core
-        layout(location = 0) uniform vec4 materialColorMultiplier;
-        layout(location = 0) out vec4 color;
+            #version 460 core
+            layout(location = 0) uniform vec4 materialColorMultiplier;
+            layout(location = 0) out vec4 color;
 
-        vec3 linearToSrgb(vec3 linearColor) {
-            bvec3 cutoff = lessThanEqual(linearColor, vec3(0.0031308));
-            vec3 lower = linearColor * 12.92;
-            vec3 upper = 1.055 * pow(linearColor, vec3(1.0 / 2.4)) - 0.055;
-            return mix(upper, lower, cutoff);
-        }
+            vec3 linearToSrgb(vec3 linearColor) {
+                bvec3 cutoff = lessThanEqual(linearColor, vec3(0.0031308));
+                vec3 lower = linearColor * 12.92;
+                vec3 upper = 1.055 * pow(linearColor, vec3(1.0 / 2.4)) - 0.055;
+                return mix(upper, lower, cutoff);
+            }
 
-        void main() {
-        #ifdef SHERKO_MANUAL_SRGB_ENCODE
-            color = vec4(
-                linearToSrgb(materialColorMultiplier.rgb),
-                materialColorMultiplier.a);
-        #else
-            color = materialColorMultiplier;
-        #endif
-        }
-        """;
+            void main() {
+            #ifdef SHERKO_MANUAL_SRGB_ENCODE
+                color = vec4(
+                    linearToSrgb(materialColorMultiplier.rgb),
+                    materialColorMultiplier.a);
+            #else
+                color = materialColorMultiplier;
+            #endif
+            }
+            """;
 
     private final OpenGlDrawBackend draw;
     private final OpenGlVertexArray vertexArray;

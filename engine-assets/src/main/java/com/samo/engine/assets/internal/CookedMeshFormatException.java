5b56a4aa3bfde5c2e8eeb714a33fb398df75f16b
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-22 14:25:27 +0300

    [P6-T07] Add cooked mesh format exception

---
+++ engine-assets/src/main/java/com/samo/engine/assets/internal/CookedMeshFormatException.java
@@ -0,0 +1,15 @@
+package com.samo.engine.assets.internal;
+
+final class CookedMeshFormatException extends RuntimeException {
+    CookedMeshFormatException(String message) {
+
+        super(message);
+
+    }
+
+    CookedMeshFormatException(String message, Throwable cause) {
+
+        super(message, cause);
+
+    }
+}

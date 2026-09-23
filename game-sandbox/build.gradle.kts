93da8eb41184129cf488c2b94822caef24ce0557
Sherko <120306123+Sherko231@users.noreply.github.com>
2026-09-24 01:08:33 +0300

    [P6-SANDBOX] Expose renderer API to sandbox tests

--- game-sandbox/build.gradle.kts
+++ game-sandbox/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(project(":engine-network-api"))
     implementation(project(":engine-ui"))
     testImplementation(project(":test-support"))
+    testCompileOnly(project(":engine-render-opengl"))
 }
 
 fun JavaExec.configureSandboxRun(mainClassName: String) {
